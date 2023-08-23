f815c82426c2740e8ee22dfe0445f985f7d6455b
Taylor Sweetman <taylor.sweetman@glia.com>
2023-08-23 10:11:40 -0400

    Set up typescript

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { simpleGit, SimpleGit, SimpleGitOptions } from 'simple-git';
+
+// const options: Partial<SimpleGitOptions> = {
+//   baseDir: process.cwd(),
+//   binary: 'git',
+//   maxConcurrentProcesses: 6,
+//   trimmed: false,
+// };
+
+// when setting all options in a single object
+// const git: SimpleGit = simpleGit(options);
+
+// // or split out the baseDir, supported for backward compatibility
+// const git: SimpleGit = simpleGit('/some/path', { binary: 'git' });
