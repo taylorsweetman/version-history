e46e5a739d65934425b8f5b3c7e139751aca05e1
Taylor Sweetman <taylor.sweetman@glia.com>
2023-08-23 10:28:37 -0400

    Show last N commits

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { simpleGit, SimpleGit, SimpleGitOptions } from 'simple-git';
 
+type SimpleLog = { message: string; sha: string };
+
+const MAX_COUNT = 2;
+
 const initGit = async (): Promise<SimpleGit> => {
   const options: Partial<SimpleGitOptions> = {
     baseDir: process.cwd(),
@@ -12,13 +16,29 @@ const initGit = async (): Promise<SimpleGit> => {
   return git;
 };
 
+const getLastNCommits = async (
+  git: SimpleGit,
+  n: number
+): Promise<SimpleLog[]> => {
+  const { all } = await git.log({
+    maxCount: n,
+  });
+
+  const simpleLogs = all.map(({ message, hash }) => ({
+    message,
+    sha: hash,
+  }));
+
+  return simpleLogs;
+};
+
 const main = async () => {
   console.log('--- Starting ---');
   const git = await initGit();
-  const { all } = await git.log();
 
-  const simpleLog = all.map((log) => log.message);
-  console.log(simpleLog);
+  const lastNCommits = await getLastNCommits(git, MAX_COUNT);
+  console.log(lastNCommits);
+  console.log('--- Complete ---');
 };
 
 main();
